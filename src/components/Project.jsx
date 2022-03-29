e6d6079a3e2a149e55e1d895fa4e03e5ea65921f
AtaDemirkiran <49242945+AtaDemirkiran@users.noreply.github.com>
2022-03-29 17:52:10 +0300

    .

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,52 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import '../App.scss'
-class Project extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isVisible: false
-        }
+const Project = ({ project }) => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toogleVisible = () => {
+
+        setVisible(!visible)
     }
 
-    // isVisibleToggle = () => {
-    //     this.setState(({
-    //         isVisible: !this.isVisible
-    //     }))
-    // }
+    return (
+        <div className="col-md-6 mb-5">
 
-    render() {
-        const { isVisible } = this.state
-        const { projects } = this.props
-        return (
+            <div className="view overlay rounded z-depth-1">
+                <img src={project.projectIMG} className="img-fluid projectImages" alt="Sample project" />
+                <a>
+                    <div className="mask rgba-white-slight"></div>
+                </a>
+            </div>
 
+            <div className="px-3 pt-3 mx-1 mt-1 pb-0">
+                <h4 className="font-weight-bold mb-3">{project.projectName}</h4>
+                <button className="btn btn-dark btn-rounded btn-md" onClick={() => toogleVisible()}>
+                    {!visible ? 'Detay Göster' : 'Detay Gizle'}
+                </button>
+                {
+                    visible ? <p className="mt-2 projectDetail "> {project.projectDetay}  </p> : null
+                }
 
-            <div className="col-md-6 mb-5">
-
-                <div className="view overlay rounded z-depth-1">
-                    <img src={projects.projectIMG} className="img-fluid projectImages" alt="Sample project" />
-                    <a>
-                        <div className="mask rgba-white-slight"></div>
-                    </a>
-                </div>
-
-                <div className="px-3 pt-3 mx-1 mt-1 pb-0">
-                    <h4 className="font-weight-bold mb-3">{projects.projectName}</h4>
-                    <button className="btn btn-dark btn-rounded btn-md" onClick={() => {
-                        this.setState({
-                            isVisible: !isVisible
-                        })
-                    }} >Detay Göster </button>
-                    {
-                        isVisible ? (
-                            <p className="mt-2 projectDetail ">{projects.projectDetay} </p>
-                        ) : null
-                    }
-                </div>
 
             </div>
 
-
-        )
-    }
+        </div>
+    )
 }
+
 export default Project
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,38 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Project from './Project'
 import saat from '../assets/images/saat.PNG'
 import proje2 from '../assets/images/proje2.PNG'
 import proje3 from '../assets/images/proje3.PNG'
-class Projects extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            data: [
-                { id: 1, projectName: 'Urun Tanıtım Sitesi', projectDetay: 'HTML/CSS/BOOTSTRAP ile yapılan bir ürün tanıtım sitesidir.', projectIMG: saat },
-                { id: 2, projectName: 'Blog Sitesi', projectDetay: 'PHP ile yaptığım bu sitede de aynı zamanda CMS Admin Paneli bulunmaktadır.', projectIMG: proje2 },
-                { id: 3, projectName: 'Futbolcu Keşif Sitesi', projectDetay: 'PHP ile yaptığımız bu sitenin ana amacanın altyapılarda keşfedilmeyi bekleyen futbolcuların öne çıkmasını sağlayan bir sitedir.', projectIMG: proje3 }
-            ]
-        }
-    }
-    render() {
-        const { data } = this.state
-        return (
-            <div className='container my-5'>
-                <section className='text-center'>
-                    <h3 className='font-weight-bold mb-4 pb-2'>Projelerim</h3>
-                    <div className='row'>
-                        {
-                            data.map(projects => {
-                                return (
-                                    <Project key={projects.id} projects={projects} />
-                                )
-                            })
-                        }
-                    </div>
-                </section>
-            </div>
-        )
-    }
+const Projects = () => {
+
+    const data = [
+        { id: 1, projectName: 'Urun Tanıtım Sitesi', projectDetay: 'HTML/CSS/BOOTSTRAP ile yapılan bir ürün tanıtım sitesidir.', projectIMG: saat },
+        { id: 2, projectName: 'Blog Sitesi', projectDetay: 'PHP ile yaptığım bu sitede de aynı zamanda CMS Admin Paneli bulunmaktadır.', projectIMG: proje2 },
+        { id: 3, projectName: 'Futbolcu Keşif Sitesi', projectDetay: 'PHP ile yaptığımız bu sitenin ana amacanın altyapılarda keşfedilmeyi bekleyen futbolcuların öne çıkmasını sağlayan bir sitedir.', projectIMG: proje3 }
+    ]
+    const [projects, setProjcets] = useState(data)
+
+    return (
+        <div className='container my-5  '>
+            <section className='text-center  projcets'>
+                <h3 className='font-weight-bold mb-4 pb-2'>Projelerim</h3>
+                <div className='row'>
+                    {
+                        projects.map(project => {
+                            return (
+                                <Project key={project.id} project={project} />
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </div>
+    )
 }
+
 export default Projects
